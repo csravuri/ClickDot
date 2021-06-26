d07af1a48ca517edcb5ba6e5b3a5f143016c9ff0
Chandra Sekhar Ravuri <chandrasekhar.r18@gmail.com>
2021-06-26 11:03:54 +0530

    Click Dot initial source

---
+++ ClickDot/ClickDot/ClickDot/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClickDot
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }        
+
+        private void DotLabel_Tapped(object sender, EventArgs e)
+        {
+            DisplayAlert("Wow!", "You clicked dot successfully.", "Do Again");
+        }
+    }
+}
